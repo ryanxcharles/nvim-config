a585eec6782ec70924b755caaa6fe71128e18212
Ryan X. Charles <ryan@ryanxcharles.com>
2024-09-30 05:57:22 -0500

    fix config cmp

--- init.lua
+++ init.lua
@@ -323,8 +323,10 @@ lspconfig.biome.setup({
   },
 })
 
+local cmp = require("cmp")
+
 -- Set up nvim-cmp (auto-complete)
-require("cmp").setup({
+cmp.setup({
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
