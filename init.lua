f8a75c5c80165f4eb3b5082efdc36b56bf02100f
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-05 16:28:19 -0500

    fix relative import of plugin by inlining it

--- init.lua
+++ init.lua
@@ -686,7 +686,58 @@ vim.g.rainbow_delimiters = {
   },
 }
 
-local CodeCompanionLualine = require("./code-companion-lualine")
+-- custom plugin to display spinner for code companion in lualina
+local function CodeCompanionLualine()
+  local M = require("lualine.component"):extend()
+
+  M.processing = false
+  M.spinner_index = 1
+
+  local spinner_symbols = {
+    "⠋",
+    "⠙",
+    "⠹",
+    "⠸",
+    "⠼",
+    "⠴",
+    "⠦",
+    "⠧",
+    "⠇",
+    "⠏",
+  }
+  local spinner_symbols_len = 10
+
+  -- Initializer
+  function M:init(options)
+    M.super.init(self, options)
+
+    local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "CodeCompanionRequest*",
+      group = group,
+      callback = function(request)
+        if request.match == "CodeCompanionRequestStarted" then
+          self.processing = true
+        elseif request.match == "CodeCompanionRequestFinished" then
+          self.processing = false
+        end
+      end,
+    })
+  end
+
+  -- Function that runs every time statusline is updated
+  function M:update_status()
+    if self.processing then
+      self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+      return spinner_symbols[self.spinner_index]
+    else
+      return nil
+    end
+  end
+
+  return M
+end
 
 -- Customize the statusline with lualine
 require("lualine").setup({
@@ -722,7 +773,7 @@ require("lualine").setup({
         always_visible = false, -- Always show diagnostics, even if 0
       },
     },
-    lualine_x = { "encoding", "fileformat", "filetype", CodeCompanionLualine }, -- Shows encoding, file format, and type
+    lualine_x = { "encoding", "fileformat", "filetype", CodeCompanionLualine() }, -- Shows encoding, file format, and type
     lualine_y = { "progress" }, -- Shows file progress (percentage through file)
     lualine_z = { "location" }, -- Shows line and column number
   },
