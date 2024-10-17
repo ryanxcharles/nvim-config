d570cc69282b526f344cc74559466e4cf6a1707f
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-17 09:24:14 -0500

    close hidden buffers. open new terminal on right.

--- init.lua
+++ init.lua
@@ -1542,3 +1542,35 @@ end
 
 -- Keybinding to reload the current Lua file
 vim.api.nvim_set_keymap("n", "<Leader>rf", ":lua ReloadCurrentFile()<CR>", opts)
+
+-- close hidden buffers. useful for aggs, argdo, ...
+function CloseHiddenBuffers()
+  local visible_buffers = {}
+  -- Get all buffers visible in the current tabs and windows
+  for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      visible_buffers[buf] = true
+    end
+  end
+
+  -- Iterate over all buffers and close the ones that are not visible
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if not visible_buffers[buf] and vim.api.nvim_buf_is_loaded(buf) then
+      vim.cmd('bd ' .. buf)
+    end
+  end
+end
+
+-- Create a command to call the function
+vim.api.nvim_create_user_command('CloseHiddenBuffers', CloseHiddenBuffers, {})
+
+-- open a terminal in a new vertical split
+vim.api.nvim_create_user_command('Term', function()
+  vim.cmd('vnew')
+  vim.cmd('term')
+  vim.cmd('wincmd R')
+end, {})
+
+-- shortcut for Term
+vim.api.nvim_set_keymap('n', '<Leader>te', ':Term<CR>', opts)
