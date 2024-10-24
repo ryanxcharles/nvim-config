25e69daaa0127c5380d5515dc8f9950f6c69a083
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-24 05:44:21 -0500

    this doesn't seem to work

--- plugin-cmp.lua
+++ plugin-cmp.lua
@@ -1,39 +1,43 @@
-local cmp = require("cmp")
+local cmp = require('cmp')
 
--- Set up nvim-cmp (auto-complete)
----@diagnostic disable-next-line: redundant-parameter
 cmp.setup({
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+      require('luasnip').lsp_expand(args.body) -- For Luasnip users.
     end,
   },
   mapping = {
-    ---@diagnostic disable-next-line: undefined-field
-    ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion manually
-    ---@diagnostic disable-next-line: undefined-field
-    ["<CR>"] = cmp.mapping(function(fallback)
-      ---@diagnostic disable-next-line: undefined-field
-      if cmp.visible() and cmp.get_selected_entry() then
-        ---@diagnostic disable-next-line: undefined-field
-        cmp.confirm({ select = false }) -- Only confirm if an item is explicitly selected
-      else
-        fallback() -- Otherwise, fallback to the default Enter behavior
-      end
-    end, { "i", "s" }), -- Supports insert and select mode
-    ---@diagnostic disable-next-line: undefined-field
-    ["<C-.>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }), -- Next item (i: insert, c: command)
-    ---@diagnostic disable-next-line: undefined-field
-    ["<C-,>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }), -- Previous item
+    ['<C-Space>'] = cmp.mapping.complete(), -- Manually trigger completion
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm the first suggestion
+    ['<Tab>'] = cmp.mapping.select_next_item(), -- Navigate to next item
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(), -- Navigate to previous item
+    ['<C-e>'] = cmp.mapping.abort(), -- Close the completion window
   },
-  sources = {
-    { name = "nvim_lsp" }, -- LSP completions (TypeScript, etc.)
-    { name = "buffer" }, -- Completions from current buffer
-    { name = "path" }, -- Path completions
-    { name = "luasnip" }, -- Snippet completions
-    { name = "npm", keyword_length = 3 }, -- NPM package completions
-  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' }, -- LSP completions
+    { name = 'buffer' },   -- Buffer completions
+    { name = 'path' },     -- Path completions
+    { name = 'luasnip' },  -- Snippet completions
+  }),
   formatting = {
-    format = require("tailwindcss-colorizer-cmp").formatter,
+    fields = { 'abbr', 'kind', 'menu' },
+    format = function(entry, item)
+      item.menu = ({
+        nvim_lsp = '[LSP]',
+        buffer = '[Buffer]',
+        path = '[Path]',
+        luasnip = '[Snippet]',
+      })[entry.source.name]
+      return item
+    end,
+  },
+})
+
+-- Set up cmdline completion
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'path' },
+    { name = 'cmdline' },
   },
 })
