943024450e386bc37021de8a1ffba3e82b80454d
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-30 05:53:36 -0500

    refine colors. remove unused color code.

--- colors.lua
+++ colors.lua
@@ -13,37 +13,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost" }, {
   end,
 })
 
--- TODO: This doesn't work - is this fixable?
--- Custom cursor color
-vim.api.nvim_set_hl(0, "Cursor", { bg = "#FA8603", fg = "#000000" })
-
--- -- Custom color for search highlighting
--- vim.api.nvim_set_hl(0, "Search", { bg = "#87af5f", fg = "#000000" })
--- -- Custom color for visual mode
--- vim.api.nvim_set_hl(0, "Visual", { bg = "#5f87af", fg = "#ffffff" })
--- -- Custom color for incremental search
--- vim.api.nvim_set_hl(0, "IncSearch", { bg = "#875fff", fg = "#ffffff" })
-
--- -- moderately bright cursor column on the highlighted window only
--- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#000000" }) -- Set this to your preferred color
--- -- vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#000000" }) -- Set this to your preferred color
--- vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
---   pattern = "*",
---   callback = function()
---     vim.api.nvim_set_hl(0, "CursorLine", { bg = "#000000" }) -- Set this to your preferred color
---     -- vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#000000" }) -- Set this to your preferred color
---     vim.wo.cursorline = true -- Enable cursor column in the active window
---     -- vim.wo.cursorcolumn = true -- Enable cursor column in the active window
---   end,
--- })
--- vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
---   pattern = "*",
---   callback = function()
---     vim.wo.cursorline = false -- Disable cursor column in inactive windows
---     -- vim.wo.cursorcolumn = false -- Disable cursor column in inactive windows
---   end,
--- })
-
 -- Set background colors for active and inactive windows
 -- Define the colors for active and inactive windows
 vim.api.nvim_set_hl(0, "ActiveWindow", { bg = "#0f0f16" }) -- Active window background color
@@ -51,8 +20,8 @@ vim.api.nvim_set_hl(0, "ActiveWindow", { bg = "#0f0f16" }) -- Active window back
 -- vim.api.nvim_set_hl(0, "InactiveWindow", { bg = "NONE" }) -- Inactive window background color
 
 -- Set Neovim background to transparent
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "#16161f" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "#16161f" })
 
 -- Autocommand for entering a window (active)
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
@@ -60,10 +29,3 @@ vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
     vim.wo.winhighlight = "Normal:ActiveWindow" -- Set active window background
   end,
 })
-
--- -- Set Telescope background color
--- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#08090c" }) -- Normal background
--- -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "#0a0a0a", fg = "#0a0a0a" })  -- Border color (make it blend)
--- -- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#0a0a0a" })  -- Prompt background
--- -- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "#0a0a0a" }) -- Results background
--- -- vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "#0a0a0a" }) -- Preview background
