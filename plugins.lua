47e4ad36d75cc8a8749d70d9810649c8583ae680
Ryan X. Charles <ryan@ryanxcharles.com>
2024-11-09 09:12:56 -0600

    move treesitter config

--- plugins.lua
+++ plugins.lua
@@ -121,7 +121,38 @@ return {
     },
     run = ":TSUpdate",
     config = function()
-      require("plugin-treesitter")
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.configs").setup({
+        -- Install parsers for various languages
+        ensure_installed = {
+          "javascript",
+          "typescript",
+          "tsx",
+          "json",
+          "jsonc",
+          "jsdoc",
+          "html",
+          "css",
+          "rust",
+          "markdown",
+          "markdown_inline",
+          "toml",
+          "wgsl",
+          "nu",
+        }, -- Add more languages as needed
+
+        -- Enable Treesitter-based syntax highlighting
+        highlight = {
+          enable = true, -- Enable Treesitter highlighting
+          additional_vim_regex_highlighting = false, -- Disable Vim's regex-based highlighting
+        },
+
+        -- You can enable more Treesitter features as needed (optional)
+        indent = { enable = true }, -- Enable Treesitter-based indentation (optional)
+
+        -- Folding
+        fold = { enable = true }, -- Enable Treesitter-based folding (optional)
+      })
     end,
   },
 
