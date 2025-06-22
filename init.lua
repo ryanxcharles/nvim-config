2a6b9736cc2651c4e169bc714b428ff2ae8d7eec
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-22 05:25:27 -0700

    move telescope config inline

--- init.lua
+++ init.lua
@@ -45,7 +45,24 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim",
     requires = { { "nvim-lua/plenary.nvim" } },
     config = function()
-      require("plugin-telescope")
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules" }, -- Exclude node_modules from typeahead matching
+          vimgrep_arguments = {
+            "rg", -- Ripgrep binary
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden", -- Search hidden files, because we use .server and .client
+            "--glob",
+            "!**/.git/**", -- Exclude the .git folder
+          },
+          -- Other default settings
+        },
+      })
     end,
   },
 
@@ -493,6 +510,7 @@ require("lazy").setup({
     end,
   },
 })
+
 -- local config_path = vim.fn.stdpath("config") .. "/lua/"
 require("globals")
 require("keybindings")
