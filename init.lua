fb29e0938e7974818fdcdaa034271cfbdf5ffba8
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-22 05:42:47 -0700

    inline plugin rust tools

--- init.lua
+++ init.lua
@@ -1112,7 +1112,52 @@ require("lazy").setup({
     "simrat39/rust-tools.nvim",
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
-      require("plugin-rust-tools")
+      local rust_tools = require("rust-tools")
+
+      local opts = { noremap = true, silent = true }
+
+      -- rust: Rust tools setup with rust-analyzer
+      rust_tools.setup({
+        server = {
+          on_attach = function(client, bufnr)
+            -- Keybindings for LSP features in Rust files
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "gd",
+              "<cmd>lua vim.lsp.buf.definition()<CR>",
+              opts
+            )
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "K",
+              "<cmd>lua vim.lsp.buf.hover()<CR>",
+              opts
+            )
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "<Leader>ca",
+              "<cmd>lua vim.lsp.buf.code_action()<CR>",
+              opts
+            )
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "<Leader>rn",
+              "<cmd>lua vim.lsp.buf.rename()<CR>",
+              opts
+            )
+          end,
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = { allFeatures = true },
+              checkOnSave = { command = "clippy" }, -- Run clippy on save
+            },
+          },
+        },
+      })
     end,
   },
 
