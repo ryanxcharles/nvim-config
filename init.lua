d00483e5bded8a64dd5d59f31e09c97c571c532f
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-30 09:10:53 -0700

    todo bg

--- init.lua
+++ init.lua
@@ -1898,7 +1898,7 @@ vim.opt.termguicolors = true
 
 -- TODO: test todo highlighting
 -- Define a highlight group for TODO comments
-vim.api.nvim_command("highlight TodoComment guifg=#FA8603 gui=bold") -- Orange color with bold
+vim.api.nvim_command("highlight TodoComment guifg=#FA8603 guibg=#111111 gui=bold") -- Orange color with bold
 -- Automatically highlight TODO comments when entering a buffer
 vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost" }, {
   group = vim.api.nvim_create_augroup("TodoHighlight", { clear = true }),
