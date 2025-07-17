46eac8d1a8126e77957fc64c791d49bdd17e5c45
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 16:38:13 -0700

    restore chatvim

---
+++ lua/plugins/chatvim.lua
@@ -0,0 +1,19 @@
+return {
+  -- Chatvim: public install
+  -- {
+  --   "chatvim/chatvim.nvim",
+  --   build = "npm install",
+  --   config = function()
+  --     require("chatvim")
+  --   end,
+  -- },
+
+  -- Chatvim: local install
+  {
+    dir = "~/dev/chatvim.nvim",
+    name = "chatvim.nvim",
+    config = function()
+      require("chatvim")
+    end,
+  },
+}
