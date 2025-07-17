0c7c56c7e66a89464aa75a8a50b0be5219cbc58e
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 12:05:55 -0700

    remove unused keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,21 +12,6 @@ vim.api.nvim_set_keymap("n", ";l", ":wincmd l<CR>", opts)
 vim.api.nvim_set_keymap("n", ";k", ":wincmd k<CR>", opts)
 vim.api.nvim_set_keymap("n", ";j", ":wincmd j<CR>", opts)
 
--- Key mappings using leader key
--- vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>h", "gT", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>l", "gt", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>n", ":tabnew<CR>", { silent = true })
--- vim.api.nvim_set_keymap("n", "<Leader>N", ":tabnew<CR><Leader>e", { silent = true })
--- vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>V", ":vsp<CR>:wincmd l<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>v", ":vsp<CR>", opts)
-
--- Keybindings for resession
--- vim.api.nvim_set_keymap("n", "<Leader>sr", ":lua require('resession').save()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>sl", ":lua require('resession').load()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<Leader>sd", ":lua require('resession').delete()<CR>", opts)
-
 -- Scroll down by 25% of the window height
 vim.api.nvim_set_keymap(
   "n",
