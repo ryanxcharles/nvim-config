163a78576eb50f1031436884e9659c099d526bed
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 05:57:55 -0700

    shell

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Globals
+vim.opt.shell = "/opt/homebrew/bin/nu"
+
 -- Window navigation
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", ";h", ":wincmd h<CR>", opts)
