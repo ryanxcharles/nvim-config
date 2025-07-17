9b8f957fbe042ad04111506592dc684d86bb513c
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 05:51:11 -0700

    restore window navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Window navigation
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", ";h", ":wincmd h<CR>", opts)
+vim.api.nvim_set_keymap("n", ";l", ":wincmd l<CR>", opts)
+vim.api.nvim_set_keymap("n", ";k", ":wincmd k<CR>", opts)
+vim.api.nvim_set_keymap("n", ";j", ":wincmd j<CR>", opts)
