a188cec67fecfa806c5b3ec493bcee6e957bcee7
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 06:25:03 -0700

    global keybindings. scroll up/down 25%.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,42 @@
 -- Add any additional keymaps here
 
 -- Globals
+local opts = { noremap = true, silent = true }
 vim.opt.shell = "/opt/homebrew/bin/nu"
 
 -- Window navigation
-local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", ";h", ":wincmd h<CR>", opts)
 vim.api.nvim_set_keymap("n", ";l", ":wincmd l<CR>", opts)
 vim.api.nvim_set_keymap("n", ";k", ":wincmd k<CR>", opts)
 vim.api.nvim_set_keymap("n", ";j", ":wincmd j<CR>", opts)
+
+-- Key mappings using leader key
+vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>h", "gT", opts)
+vim.api.nvim_set_keymap("n", "<Leader>l", "gt", opts)
+vim.api.nvim_set_keymap("n", "<Leader>n", ":tabnew<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>N", ":tabnew<CR><Leader>e", { silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>V", ":vsp<CR>:wincmd l<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>v", ":vsp<CR>", opts)
+
+-- Keybindings for resession
+vim.api.nvim_set_keymap("n", "<Leader>ss", ":lua require('resession').save()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>sl", ":lua require('resession').load()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>sd", ":lua require('resession').delete()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>sc", ":lua require('resession').autosave_toggle()<CR>", opts)
+
+-- Scroll down by 25% of the window height
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>j",
+  ":lua vim.cmd('normal! ' .. math.floor(vim.fn.winheight(0) * 0.25) .. 'jzz')<CR>",
+  opts
+)
+-- Scroll up by 25% of the window height
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>k",
+  ":lua vim.cmd('normal! ' .. math.floor(vim.fn.winheight(0) * 0.25) .. 'kzz')<CR>",
+  opts
+)
--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -4,12 +4,6 @@ return {
     opts = {},
     config = function()
       require("resession").setup({})
-      -- Keybindings for resession
-      local opts = { noremap = true, silent = true }
-      vim.api.nvim_set_keymap("n", "<Leader>ss", ":lua require('resession').save()<CR>", opts)
-      vim.api.nvim_set_keymap("n", "<Leader>sl", ":lua require('resession').load()<CR>", opts)
-      vim.api.nvim_set_keymap("n", "<Leader>sd", ":lua require('resession').delete()<CR>", opts)
-      vim.api.nvim_set_keymap("n", "<Leader>sc", ":lua require('resession').autosave_toggle()<CR>", opts)
     end,
   },
 }
