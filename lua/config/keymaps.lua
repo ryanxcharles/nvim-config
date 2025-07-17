0dfe100bef147fd6cba3cd6a2fee9e19aa872eb6
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 10:08:21 -0700

    <leader>chb

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -41,3 +41,27 @@ vim.api.nvim_set_keymap(
   ":lua vim.cmd('normal! ' .. math.floor(vim.fn.winheight(0) * 0.25) .. 'kzz')<CR>",
   opts
 )
+
+-- close hidden buffers. useful for aggs, argdo, ...
+function CloseHiddenBuffers()
+  local visible_buffers = {}
+  -- Get all buffers visible in the current tabs and windows
+  for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      visible_buffers[buf] = true
+    end
+  end
+
+  -- Iterate over all buffers and close the ones that are not visible
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if not visible_buffers[buf] and vim.api.nvim_buf_is_loaded(buf) then
+      vim.cmd("bd " .. buf)
+    end
+  end
+end
+
+-- Create a command to call the function
+vim.api.nvim_create_user_command("CloseHiddenBuffers", CloseHiddenBuffers, {})
+
+vim.api.nvim_set_keymap("n", "<Leader>chb", ":CloseHiddenBuffers<CR>", {})
