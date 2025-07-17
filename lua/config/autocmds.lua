31fe11b0cf9c4ae4ecc04bd95b0da6db3dc22549
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-17 16:02:42 -0700

    restore border colors

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,26 @@
 -- Make active window more visible
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "#222434" }) -- inactive window background
 vim.api.nvim_set_hl(0, "Normal", { bg = "#161626" }) -- active window background
+vim.api.nvim_set_hl(0, "WinSeparatorNC", { fg = "#cae797", bg = "#222434" }) -- inactive window separator
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#cae797", bg = "#222434" }) -- active window separator
+vim.opt.winhl = "WinSeparator:WinSeparator"
+-- Enable window borders globally
+vim.opt.number = true -- This helps with left border visibility
+vim.opt.relativenumber = true -- Optional
+vim.opt.signcolumn = "yes" -- This helps ensure left border space
+vim.opt.foldcolumn = "1" -- This can help with left border too
+-- Create autocmd for window focus
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter", "WinLeave", "BufLeave" }, {
+  callback = function()
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      if w == vim.api.nvim_get_current_win() then
+        -- Current window gets highlighted border and background
+        vim.wo[w].winhighlight = "WinSeparator:WinSeparator,Normal:Normal"
+      else
+        -- Other windows get dim border and background
+        vim.wo[w].winhighlight = "WinSeparator:WinSeparatorNC,Normal:NormalNC"
+      end
+    end
+  end,
+})
